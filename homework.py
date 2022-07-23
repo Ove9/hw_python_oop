d24ff1e2141409761259d9cf36a39dfc5c9658f3
Ove9 <103597283+Ove9@users.noreply.github.com>
2022-07-23 21:13:59 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -15,10 +15,10 @@ class InfoMessage:
 
     def get_message(self) -> str:
         message: str = (f'Тип тренировки: {self.training_type}; '
-                   f'Длительность: {self.duration:.3f} ч.; '
-                   f'Дистанция: {self.distance:.3f} км; '
-                   f'Ср. скорость: {self.speed:.3f} км/ч; '
-                   f'Потрачено ккал: {self.calories:.3f}. ')
+                        f'Длительность: {self.duration:.3f} ч.; '
+                        f'Дистанция: {self.distance:.3f} км; '
+                        f'Ср. скорость: {self.speed:.3f} км/ч; '
+                        f'Потрачено ккал: {self.calories:.3f}. ')
         return message
 
 
